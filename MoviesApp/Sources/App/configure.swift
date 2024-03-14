596e57568a964639202b69129c2ecf6c8cd031cd
Roman Indermühle <roman.indermuehle@icloud.com>
2024-03-14 21:40:47 +0100

    Changed db name and controller

--- MoviesApp/Sources/App/configure.swift
+++ MoviesApp/Sources/App/configure.swift
@@ -9,13 +9,13 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     try app.databases.use(DatabaseConfigurationFactory.mongo(
-        connectionString: Environment.get("DATABASE_URL") ?? "mongodb://localhost:27017/vapor_database"
+        connectionString: Environment.get("DATABASE_URL") ?? "mongodb://localhost:27017/brainBox_database"
     ), as: .mongo)
     
     //register controllers
     try app.register(collection: MoviesController())
     
-    try app.register(collection: PeopleController())
+    try app.register(collection: DirectorController())
     
     // register routes
     try routes(app)
