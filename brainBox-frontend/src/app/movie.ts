a29b3571fa53d706fba86b83fc64fbef97894c3e
Roman Indermühle <roman.indermuehle@icloud.com>
2024-03-23 22:36:50 +0100

    Movie interface added

---
+++ brainBox-frontend/src/app/movie.ts
@@ -0,0 +1,13 @@
+import { UUID } from "crypto";
+
+export interface Movie {
+  id: UUID
+  title: string
+  release_year: number
+
+  production_costs: string
+
+  genre: [string]
+
+  director: UUID
+}
