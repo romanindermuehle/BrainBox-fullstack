bd5256cb4cc7643c43d6d7de369752fd328c94dd
Roman Indermühle <roman.indermuehle@icloud.com>
2024-03-23 22:38:01 +0100

    MovieService dependency added

--- brainBox-frontend/src/app/app.component.ts
+++ brainBox-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'brainBox-frontend';
+
+  constructor(private service: MovieService) {}
+
+  movies = this.service.logMovies();
+
 }
